b4b6263d636afbb9e737372e57ace88ae863755b
Arianna <87552392+ar14nn422@users.noreply.github.com>
2023-06-11 15:09:32 -0600

    Update MAIN.cpp

--- MAIN.cpp
+++ MAIN.cpp
@@ -2,12 +2,48 @@
 #include"VIAJE.h"
 #include "coleccion.h"
 int main(){
-	coleccion dias;
+	Coleccion dias;
 	viaje v1;
-	
-	v1.solicitarDatos();
-	dias.mostrarDetalleViajes(v1);
+	int diaMes, ltrConsumidos;
+	float horaFin, horaInicio,km;
+	string lugarOrigen, lugarDestino;
+		cout<<"Enter para ingresar un viaje..."<<endl;
+		getchar();
+		
+		cout << "Ingrese el día del mes(1-31): ";
+		cin >> diaMes;
+		cin.ignore();
+		
+		cout << "Ingrese el lugar de origen: ";
+		cin.ignore();
+		getline(cin, lugarOrigen);
+		
+		cout << "Ingrese el lugar de destino: ";
+		getline(cin, lugarDestino);
+		
+		cout << "Ingrese los litros consumidos: ";
+		cin >> ltrConsumidos;
+		
+		
+		cout << "Ingrese los kilómetros recorridos: ";
+		cin >> km;
+		
+		cout << "Ingrese la hora de inicio(HH,MM): ";
+		cin >> horaInicio;
+		cin.ignore();
+		
+		cout << "Ingrese la hora de fin(HH,MM): ";
+		cin >> horaFin;
+		cin.ignore();
+		
+		
+		cout<<endl;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	//v1.solicitarDatos();
+	dias.insertar(v1);
 	cout<<v1.toString();
+
 
 	
 	return 0;
